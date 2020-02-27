0224f51cd056cde73f2b3b9096e58f345f037990
GhostKicker <32245728+GhostKicker@users.noreply.github.com>
2020-02-28 00:23:06 +0300

    first commit

---
+++ fadeev_a_u/lab01/Part1/part1.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+using namespace cv;
+
+int main() {
+    int h = 60;
+    int w = 3 * 256;
+    Mat img(Mat::zeros(h, w, CV_8U));
+    Mat imgres(Size(w, 2 * h), CV_8U);
+    for (int col = 0; col < w; ++col) {
+        for (int row = 0; row < h; ++row) {
+            img.at<uchar>(Point(col, row)) = col / 3;
+        }
+    }
+    img.copyTo(imgres(Rect(0, 0, w, h)));
+    img.convertTo(img, CV_64F, 1.0 / 256);
+    cv::pow(img, 2.3, img);
+    img.convertTo(img, CV_8U, 256);
+    img.copyTo(imgres(Rect(0, h, w, h)));
+
+
+    namedWindow("a", 1);
+    imshow("a", imgres);
+    waitKey(0);
+
+
+
+}
---
+++ fadeev_a_u/lab02/lab02/Source.cpp
@@ -0,0 +1,52 @@
+#include <opencv2/opencv.hpp>
+#include <bits/stdc++.h>
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat img = imread("pic.png");
+    imwrite("pic_good.jpeg", img, { IMWRITE_JPEG_QUALITY, 95 });
+    imwrite("pic_bad.jpeg", img, { IMWRITE_JPEG_QUALITY, 65 });
+    Mat img_good = imread("pic_good.jpeg");
+    Mat img_bad = imread("pic_bad.jpeg");
+
+
+    int cols = img.cols;
+    int rows = img.rows;
+    Mat result(rows * 3, cols * 4, CV_8UC3);
+    img.copyTo(result(Rect2d(0, 0, rows, cols)));
+    img_good.copyTo(result(Rect2d(0, rows, rows, cols)));
+    img_bad.copyTo(result(Rect2d(0, 2*rows, rows, cols)));
+
+    array<Mat, 3> ar;
+    split(img, ar);
+
+
+
+    for (int i = 0; i < 3; ++i) {
+        Mat tmp;
+        merge(array<Mat, 3>({ ar[i], ar[i], ar[i] }), tmp);
+        tmp.copyTo(result(Rect2d((i+1) * cols, 0, rows, cols)));
+    }
+
+    array<Mat, 3> for_splitting;
+
+    split(img_good, for_splitting);
+    for (int i = 0; i < 3; ++i) {
+        Mat tmp;
+        absdiff(for_splitting[i], ar[i], tmp);
+        merge(array<Mat, 3>({ tmp, tmp, tmp }), tmp);
+        tmp.copyTo(result(Rect2d((i + 1) * cols, rows, rows, cols)));
+    }
+
+    split(img_bad, for_splitting);
+    for (int i = 0; i < 3; ++i) {
+        Mat tmp;
+        absdiff(for_splitting[i], ar[i], tmp);
+        merge(array<Mat, 3>({ tmp, tmp, tmp }), tmp);
+        tmp.copyTo(result(Rect2d((i + 1) * cols, 2*rows, rows, cols)));
+    }
+
+    imshow("qwer", result);
+    waitKey(0);
+}
