6343ea885c2a04c2e3bb23bae76993ce4fcae1ce
GhostKicker <32245728+GhostKicker@users.noreply.github.com>
2020-03-10 11:46:43 +0300

    пппп

--- fadeev_a_u/lab02/lab02/Source.cpp
+++ fadeev_a_u/lab02/lab02/Source.cpp
@@ -13,10 +13,10 @@ int main() {
 
     int cols = img.cols;
     int rows = img.rows;
-    Mat result(rows * 3, cols * 4, CV_8UC3);
-    img.copyTo(result(Rect2d(0, 0, rows, cols)));
-    img_good.copyTo(result(Rect2d(0, rows, rows, cols)));
-    img_bad.copyTo(result(Rect2d(0, 2*rows, rows, cols)));
+    Mat result(rows * 3, cols * 5, CV_8UC3);
+    img.copyTo(result(Rect2d(0, 0, cols, rows)));
+    img_good.copyTo(result(Rect2d(0, rows, cols, rows)));
+    img_bad.copyTo(result(Rect2d(0, 2*rows, cols, rows)));
 
     array<Mat, 3> ar;
     split(img, ar);
@@ -26,7 +26,7 @@ int main() {
     for (int i = 0; i < 3; ++i) {
         Mat tmp;
         merge(array<Mat, 3>({ ar[i], ar[i], ar[i] }), tmp);
-        tmp.copyTo(result(Rect2d((i+1) * cols, 0, rows, cols)));
+        tmp.copyTo(result(Rect2d((i+1) * cols, 0, cols, rows)));
     }
 
     array<Mat, 3> for_splitting;
@@ -36,7 +36,7 @@ int main() {
         Mat tmp;
         absdiff(for_splitting[i], ar[i], tmp);
         merge(array<Mat, 3>({ tmp, tmp, tmp }), tmp);
-        tmp.copyTo(result(Rect2d((i + 1) * cols, rows, rows, cols)));
+        tmp.copyTo(result(Rect2d((i + 1) * cols, rows, cols, rows)));
     }
 
     split(img_bad, for_splitting);
@@ -44,8 +44,23 @@ int main() {
         Mat tmp;
         absdiff(for_splitting[i], ar[i], tmp);
         merge(array<Mat, 3>({ tmp, tmp, tmp }), tmp);
-        tmp.copyTo(result(Rect2d((i + 1) * cols, 2*rows, rows, cols)));
+        tmp.copyTo(result(Rect2d((i + 1) * cols, 2*rows, cols, rows)));
     }
+
+    Mat grayimg, grayjgood, grayjbad;
+    cvtColor(img, grayimg, COLOR_BGR2GRAY);
+    merge(array<Mat, 3>({ grayimg, grayimg, grayimg }), grayimg);
+    cvtColor(img_good, grayjgood, COLOR_BGR2GRAY);
+    merge(array<Mat, 3>({ grayjgood, grayjgood, grayjgood }), grayjgood);
+    cvtColor(img_bad, grayjbad, COLOR_BGR2GRAY);
+    merge(array<Mat, 3>({ grayjbad, grayjbad, grayjbad }), grayjbad);
+    absdiff(grayimg, grayjgood, grayjgood);
+    absdiff(grayimg, grayjbad, grayjbad);
+    grayimg.copyTo(result(Rect2d(cols * 4, 0, cols, rows)));
+    grayjgood.copyTo(result(Rect2d(cols * 4, rows, cols, rows)));
+    grayjbad.copyTo(result(Rect2d(cols * 4, 2 * rows, cols, rows)));
+
+
 
     imshow("qwer", result);
     waitKey(0);
--- fadeev_a_u/lab03/lab03/lab03.cpp
+++ fadeev_a_u/lab03/lab03/lab03.cpp
@@ -1,8 +1,7 @@
 #include <vector>
 #include <iostream>
-#include <bits/stdc++.h>
 #include <opencv2/opencv.hpp>
-
+#include <opencv2/imgproc.hpp>
 using namespace cv;
 using namespace std;
 
@@ -32,7 +31,7 @@ int main() {
 
 
     Mat histviz = Mat::zeros(256, 256, CV_8U);
-    for (int col = 0; col < hist.size(); ++col) {
+    for (int col = 0; col < (int)hist.size(); ++col) {
         for (int row = 0; row < hist[col]; ++row) {
             histviz.at<uchar>(255 - row, col) = 255;
         }
@@ -44,27 +43,42 @@ int main() {
         //lookup[i] = pow(1.0 * i / 256, 2) * 256;
     }
     Mat lookupviz = Mat::zeros(256, 256, CV_8U);
-    for (int i = 0; i < lookup.size(); ++i) lookupviz.at<uchar>(255 - lookup[i], i) = 255;
+    for (int i = 0; i < (int)lookup.size(); ++i) lookupviz.at<uchar>(255 - lookup[i], i) = 255;
 
     Mat pic2;
     LUT(pic, lookup, pic2);
     auto hist2 = get_hist(pic2);
 
     Mat histviz2 = Mat::zeros(256, 256, CV_8U);
-    for (int col = 0; col < hist2.size(); ++col) {
+    for (int col = 0; col < (int)hist2.size(); ++col) {
         for (int row = 0; row < hist2[col]; ++row) {
             histviz2.at<uchar>(255 - row, col) = 255;
         }
     }
-    Mat piceq;
-    equalizeHist(pic, piceq);
+
+
+    Mat pic_clahe1, pic_clahe2, pic_clahe3;
+    createCLAHE(10, Size(8, 8))->apply(pic, pic_clahe1);
+    createCLAHE(10, Size(2, 8))->apply(pic, pic_clahe2);
+    createCLAHE(10, Size(8, 2))->apply(pic, pic_clahe3);
+
+    Mat globalbin;
+    threshold(pic, globalbin, 80, 255, CV_8U);
+
+    Mat localbin;
+
 
 
     imshow("function", lookupviz);
     imshow("orginal hist", histviz);
     imshow("after func hist", histviz2);
     imshow("original", pic);
-    imshow("equalized", piceq);
     imshow("func", pic2);
+    imshow("clahe 10 8 8", pic_clahe1);
+    imshow("clahe 10 2 8", pic_clahe2);
+    imshow("clahe 10 8 2", pic_clahe3);
+    imshow("global binarization", globalbin);
+
+
     waitKey(0);
 }
